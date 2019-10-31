1b71f92c1565b487e65e427978611f86f1df39b7
Rik Meijer <github@rikmeijer.nl>
2019-10-31 15:35:47 +0100

    refactor APITest
    Signed-off-by: Rik Meijer <github@rikmeijer.nl>

--- tests/APITest.php
+++ tests/APITest.php
@@ -14,23 +14,22 @@ class APITest extends TestCase
         $this->assertTrue(true);
     }
 
-    private function assertJSONResponse(ResponseInterface $response) {
+    private function assertJSONResponse(string $expectedJson, ResponseInterface $response) : void {
         $this->assertEquals('application/json', $response->getHeader('Content-Type')[0]);
+        $this->assertJson($expectedJson, $response->getBody()->__toString());
     }
 
     final public function testAPIhandleServerRequest_When_GETDefaultURI_Expect_200Response(): void
     {
         $response = API::handleServerRequest(new ServerRequest('GET', '/.well-known/query'));
         $this->assertEquals(200, $response->getStatusCode());
-        $this->assertJSONResponse($response);
-        $this->assertJson('{}', $response->getBody()->__toString());
+        $this->assertJsonResponse('{}', $response);
     }
 
     final public function testAPIhandleServerRequest_When_POSTCreateQuery_Expect_201Response(): void
     {
         $response = API::handleServerRequest(new ServerRequest('POST', '/.well-known/query?q=hello'));
         $this->assertEquals(201, $response->getStatusCode());
-        $this->assertJSONResponse($response);
-        $this->assertJson('{}', $response->getBody()->__toString());
+        $this->assertJsonResponse('{}', $response);
     }
 }
