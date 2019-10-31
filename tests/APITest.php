70efcbc15d234ddb4b93ea9d3a236b0cf50cc2de
Rik Meijer <github@rikmeijer.nl>
2019-10-31 14:23:44 +0100

    init phpunit and sanity test
    Signed-off-by: Rik Meijer <github@rikmeijer.nl>

---
+++ tests/APITest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace rikmeijer\WebQuery;
+
+use PHPUnit\Framework\TestCase;
+
+class APITest extends TestCase
+{
+    final public function testSanity() : void
+    {
+        $this->assertTrue(true);
+    }
+}
